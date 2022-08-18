bc9d89d3b6e1ada4d3278b027edb9fb5db982812
sanihaseeb <sanihaseeb@hotmail.com>
2022-08-18 21:08:57 +0000

    Add order filling simulation

--- src/main/java/com/conygre/spring/boot/AppConfig.java
+++ src/main/java/com/conygre/spring/boot/AppConfig.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Import(SwaggerConfig.class)
 @ComponentScan
+@EnableScheduling
 public class AppConfig {
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
