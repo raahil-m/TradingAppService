b88061ac2b40d96573b31caee54a3befc6e49ffe
sanihaseeb <sanihaseeb@hotmail.com>
2022-08-24 19:24:39 +0000

    Check fix

--- src/main/java/com/conygre/spring/boot/services/UserServicelmpl.java
+++ src/main/java/com/conygre/spring/boot/services/UserServicelmpl.java
@@ -1,24 +1,24 @@
-package com.conygre.spring.boot.services;
-
-import com.conygre.spring.boot.entities.Stock;
-import com.conygre.spring.boot.entities.User;
-import com.conygre.spring.boot.repos.StockRepository;
-import com.conygre.spring.boot.repos.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class UserServicelmpl implements UserService{
-    @Autowired
-    private UserRepository dao;
-    @Override
-    public String getUserName(int user_id) {
-
-        User user = dao.findById(user_id).get();
-        return user.getUsername();
-    }
-
-    @Override
-    public Double getBalance(int user_id) {
-        User user = dao.findById(user_id).get();
-        return user.getBalance();
-    }
-}
+//package com.conygre.spring.boot.services;
+//
+//import com.conygre.spring.boot.entities.Stock;
+//import com.conygre.spring.boot.entities.User;
+//import com.conygre.spring.boot.repos.StockRepository;
+//import com.conygre.spring.boot.repos.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//public class UserServicelmpl implements UserService{
+//    @Autowired
+//    private UserRepository dao;
+//    @Override
+//    public String getUserName(int user_id) {
+//
+//        User user = dao.findById(user_id).get();
+//        return user.getUsername();
+//    }
+//
+//    @Override
+//    public Double getBalance(int user_id) {
+//        User user = dao.findById(user_id).get();
+//        return user.getBalance();
+//    }
+//}
