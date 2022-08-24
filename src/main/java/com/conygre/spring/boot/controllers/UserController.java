7bc971608bac1d3fb62785dcc84b1be19f957573
Administrator <kyeobj@gmail.com>
2022-08-24 19:04:46 +0000

    UserController added

--- src/main/java/com/conygre/spring/boot/controllers/UserController.java
+++ src/main/java/com/conygre/spring/boot/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.conygre.spring.boot.controllers;
 
 import com.conygre.spring.boot.entities.Stock;
 import com.conygre.spring.boot.services.StockService;
+import com.conygre.spring.boot.services.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,19 @@ import java.util.List;
 @CrossOrigin // allows requests from all domains
 @RequestMapping("/api/user")
 public class UserController {
-    private static Logger logger = LogManager.getLogger(StockController.class);
+    private static Logger logger = LogManager.getLogger(UserController.class);
     @Autowired
-    private StockService service;
+    private UserService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    String getUserName(int user_id) {
+        return service.getUserName(user_id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    Double getBalance(int user_id) {
+        return service.getBalance(user_id);
+    }
 
 
 }
